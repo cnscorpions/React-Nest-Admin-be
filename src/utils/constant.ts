ad01d1fce14c9baaae44900e34ac8999cab26c81
gavin <justin.project@yahoo.com>
2020-03-12 19:55:35 +0800

    fix cannot find MIMETYPE

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,6 +4,6 @@ const JWT_EXPIRED = 60 * 60 * 24; // 1 day
 
 // ebook
 const UPLOAD_DIR = process.env.NODE_ENV === "production" ? "/home/gavin/react-nest-admin/upload" : "upload/";
-MIME_TYPE_EPUB = "application/epub+zip";
+const MIME_TYPE_EPUB = "application/epub+zip";
 
 export { PRIVATE_KEY, JWT_EXPIRED, UPLOAD_DIR, MIME_TYPE_EPUB };
