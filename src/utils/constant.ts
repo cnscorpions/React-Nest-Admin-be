04274a6c07c4b8aa62cd9720413d2240d50aab9f
Chan Gavin <justin.coding@gmail.com>
2021-04-24 18:40:29 +0800

    add docs folder

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,7 +3,10 @@ const PRIVATE_KEY = "a_token_for_develop";
 const JWT_EXPIRED = 60 * 60 * 24; // 1 day
 
 // ebook
-const UPLOAD_DIR = process.env.NODE_ENV === "production" ? "/home/gavin/react-nest-admin/upload" : "upload/";
+const UPLOAD_DIR =
+  process.env.NODE_ENV === "production"
+    ? "/home/gavin/react-nest-admin/upload"
+    : "upload/";
 // const MIME_TYPE_EPUB = "application/epub+zip";
 
 export { PRIVATE_KEY, JWT_EXPIRED, UPLOAD_DIR };
